e4863ca6bbe9876666eea866b615175b2189f114
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-08-11 20:09:33 +0530

    lettuce: Move loggy to post-fs and record all buffers

--- rootdir/etc/loggy.sh
+++ rootdir/etc/loggy.sh
@@ -2,5 +2,5 @@
 # loggy.sh.
 
 _date=`date +%F_%H-%M-%S`
-logcat -v time -f  /cache/logcat_${_date}.txt
+logcat -v -b all time -f  /cache/logcat_${_date}.txt
 cat /proc/kmsg > /cache/kmsg_${_date}.txt
