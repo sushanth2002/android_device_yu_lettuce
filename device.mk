e5a7dbebda938dbbe4aba164c46007008138be6a
Pranav Vashi <neobuddy89@gmail.com>
2018-09-11 03:55:34 +0530

    lettuce: Whitelist IMS.apk

--- device.mk
+++ device.mk
@@ -334,7 +334,8 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/vendor/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/vendor/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfigs/qti_whitelist.xml
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
